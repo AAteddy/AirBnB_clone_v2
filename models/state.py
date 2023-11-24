88631a543dc72827ed400dd183a66bd16b97a130
Tedros <tedsaasfaha@gmail.com>
2023-11-24 04:10:41 -0800

    state.py file updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,48 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+from models import storage
+from os import getenv
+from models.base_model import Base
 from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+
+    __tablename__ = "states"
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all,delete", backref="state")
+    else:
+        name = ""
+        # DONE: for FileStorage: getter attribute cities that
+        # returns the list of City instances with state_id equals
+        # to the current State.id => It will be the FileStorage
+        # relationship between State and City
+
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            from models import storage
+
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
