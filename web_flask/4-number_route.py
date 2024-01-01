c82f1fada4dc5a9e4295180f1ba3131ed854c4c1
Tedros <tedsaasfaha@gmail.com>
2024-01-01 07:42:54 -0800

    4-number_route.py file updated

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,9 +5,9 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
     /hbnb: Displays 'HBNB"
-    /c/<text>: Displays “C ” followed by the value of the text variable
+    /c/<text>: Displays “C ” followed by the value of the variable
                (replace underscore _ symbols with a space ).
-    /python/<text>: Displays “Python ”, followed by the value of the text variable.
+    /python/<text>: Displays “Python”, followed by the value of the variable
                (replace underscore _ symbols with a space ).
                The default value of text is “is cool”.
     /number/<n>: Displays “n is a number” only if n is an integer.
@@ -20,13 +20,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!' """
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB' """
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
